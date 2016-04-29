2fe70d780047a1072b848ac590a9d760cf028931
王森俊 <291876969@qq.com>
2016-04-29 22:32:25 +0800

    2
    2

--- js/1ye.js
+++ js/1ye.js
@@ -4,8 +4,8 @@
 $(document).ready(function() {
 r = function() {
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/image-1008.png' : 'images/image-672.png') : 'images/image-336.png');
-var a='data-lazy'; if($('.gallery-2 .slide0').hasAttr('src')) { a='src'; } $('.gallery-2 .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/new-1500.png' : 'images/new-1000.png') : 'images/new-500.png');
-var a='data-lazy'; if($('.gallery-2 .slide1').hasAttr('src')) { a='src'; } $('.gallery-2 .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/zhan-wei-fu-1500.jpg' : 'images/zhan-wei-fu-1000.jpg') : 'images/zhan-wei-fu-500.jpg');};
+var a='data-lazy'; if($('.gallery-2 .slide0').hasAttr('src')) { a='src'; } $('.gallery-2 .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/new-1500-2.png' : 'images/new-1000-2.png') : 'images/new-500-2.png');
+var a='data-lazy'; if($('.gallery-2 .slide1').hasAttr('src')) { a='src'; } $('.gallery-2 .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/zhan-wei-fu-1500-2.jpg' : 'images/zhan-wei-fu-1000-2.jpg') : 'images/zhan-wei-fu-500-2.jpg');};
 $(window).resize(r);
 r();
 $('.gallery-2 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: true,infinite: true});
