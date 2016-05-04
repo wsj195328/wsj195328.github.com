768e00dbaa75ff10d8b3abf068c5e1f3518c025b
王森俊 <291876969@qq.com>
2016-05-04 16:42:10 +0800

    2
    2

--- js/index.js
+++ js/index.js
@@ -3,12 +3,8 @@
 
 $(document).ready(function() {
 r = function() {
-var a='data-lazy'; if($('.img').hasAttr('src')) { a='src'; } $('.img').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ming-ren-2880.jpg' : 'images/ming-ren-1920.jpg') : 'images/ming-ren-960.jpg');
-$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/bgirl-1053.jpg' : 'images/bgirl-702.jpg') : 'images/bgirl-351.jpg');};
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/bgirl-1053.jpg' : 'images/bgirl-702.jpg') : 'images/bgirl-351.jpg');};
 $(window).resize(r);
 r();
-$('.anchor').mouseenter(function() { $.loadImages((window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ming-ren-2400.jpg' : 'images/ming-ren-1600.jpg') : 'images/ming-ren-800.jpg', function() { }) });
-$('.anchor').magnificPopup({ type: 'image', closeOnContentClick: true, closeBtnInside: false, mainClass: 'mfp-no-margins mfp-with-zoom', image: { verticalFit: true }, zoom: { enabled: true, duration: 300 }, callbacks: { elementParse: function(item) { item.src = (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ming-ren-2400.jpg' : 'images/ming-ren-1600.jpg') : 'images/ming-ren-800.jpg'; } } });
-$('.img').unveil(50);
 
 });
